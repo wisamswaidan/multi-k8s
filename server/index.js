17281b3a441de395d34cbbe10b0989b57ea7068b
Wisam Swaidan <wisamswaidan@MacBook-Pro-tilhrende-Wisam.local>
2021-02-17 17:35:16 +0100

    Done with the course

--- server/index.js
+++ server/index.js
@@ -40,9 +40,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  // const values = await pgClient.query('SELECT * from values');
+  const values = await pgClient.query('SELECT * from values');
 
-  res.send([1, 2, 3]);
+  res.send(values.rows);
+  
 });
 
 app.get('/values/current', async (req, res) => {
